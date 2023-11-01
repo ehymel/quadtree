41783b8758404c7deb10a9adff240b78d02d351c
E Hymel <ehymel@practicetally.com>
2023-10-31 21:17:37 -0500

    Start on quadtree functions

--- quadtree.js
+++ quadtree.js
@@ -14,7 +14,30 @@ class Rectangle {
 }
 
 class QuadTree {
-    constuctor(boundary) {
+    constructor(boundary, capacity) {
         this.boundary = boundary;
+        this.capacity = capacity;
+        this.points = [];
+        this.divided = false;
+    }
+
+    insert(point) {
+        if (this.points.length < this.capacity) {
+            this.points.push(point);
+            return;
+        }
+
+        this.subdivide();
+    }
+
+    subdivide() {
+        let ne = new Rectangle(this.boundary.x + this.boundary.w/2, this.boundary.y + this.boundary.h/2, this.boundary.w/2, this.boundary.h/2);
+        this.northEast = new QuadTree(ne, this.capacity);
+        let nw = new Rectangle(this.boundary.x - this.boundary.w/2, this.boundary.y + this.boundary.h/2, this.boundary.w/2, this.boundary.h/2);
+        this.northWest = new QuadTree(nw, this.capacity);
+        let sw = new Rectangle(this.boundary.x - this.boundary.w/2, this.boundary.y - this.boundary.h/2, this.boundary.w/2, this.boundary.h/2);
+        this.southWest = new QuadTree(sw, this.capacity);
+        let se = new Rectangle(this.boundary.x + this.boundary.w/2, this.boundary.y - this.boundary.h/2, this.boundary.w/2, this.boundary.h/2);
+        this.southEast = new QuadTree(se, this.capacity);
     }
 }
--- sketch.js
+++ sketch.js
@@ -4,8 +4,12 @@ function setup() {
     // initial boundary should include the entire canvas
     // center of initial boundary is center of canvas
     let boundary = new Rectangle(width/2, height/2, width/2, height/2);
-    let qt = new QuadTree(boundary);
+    let qt = new QuadTree(boundary, 4);
 
+    for (let i = 0; i < 1; i++) {
+        let p = new Point(random(width), random(height));
+        qt.insert(p);
+    }
 }
 
 function draw() {
