a7e6e0b02f2abe0532036ea5a3913bd392642cf6
E Hymel <ehymel@practicetally.com>
2023-11-02 19:16:18 -0500

    Don't insert into divided container

--- quadtree.js
+++ quadtree.js
@@ -13,10 +13,10 @@ class Rectangle {
     }
 
     contains(point) {
-        return this.x - w/2 > point.location.x ||
-            this.x + w/2 > point.location.x ||
-            this.y - y/2 < point.location.y ||
-            this.y + y/2 > point.location.y;
+        return point.location.x >= this.x - this.w &&
+            point.location.x < this.x + this.w &&
+            point.location.y <= this.y + this.h &&
+            point.location.y > this.y - this.h;
     }
 }
 
@@ -33,20 +33,32 @@ class QuadTree {
             return;
         }
 
+        if (this.divided) {
+            this.insertIntoDivisions(point);
+        }
+
         if (this.points.length < this.capacity) {
             this.points.push(point);
             return;
         }
 
-        if (!this.divided) {
-            this.subdivide();
-            this.divided = true;
+        this.subdivide();
+
+        // re-distribute points of this container to its subdivisions
+        let pts = this.points;
+        this.points = [];
+        for (let p of pts) {
+            this.insertIntoDivisions(p);
         }
 
+        this.insertIntoDivisions(point);
+    }
+
+    insertIntoDivisions(point) {
+        this.northWest.insert(point);
         this.northEast.insert(point);
         this.southEast.insert(point);
         this.southWest.insert(point);
-        this.northWest.insert(point);
     }
 
     subdivide() {
@@ -55,13 +67,35 @@ class QuadTree {
         let w = this.boundary.w;
         let h = this.boundary.h;
 
-        let ne = new Rectangle(x + w/2, y + h/2, w/2, h/2);
-        this.northEast = new QuadTree(ne, this.capacity);
-        let nw = new Rectangle(x - w/2, y + h/2, w/2, h/2);
+        let nw = new Rectangle(x - w/2, y - h/2, w/2, h/2);
         this.northWest = new QuadTree(nw, this.capacity);
-        let sw = new Rectangle(x - w/2, y - h/2, w/2, h/2);
-        this.southWest = new QuadTree(sw, this.capacity);
-        let se = new Rectangle(x + w/2, y - h/2, w/2, h/2);
+        let ne = new Rectangle(x + w/2, y - h/2, w/2, h/2);
+        this.northEast = new QuadTree(ne, this.capacity);
+        let se = new Rectangle(x + w/2, y + h/2, w/2, h/2);
         this.southEast = new QuadTree(se, this.capacity);
+        let sw = new Rectangle(x - w/2, y + h/2, w/2, h/2);
+        this.southWest = new QuadTree(sw, this.capacity);
+
+        this.divided = true;
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(1);
+        noFill();
+        rectMode(CENTER);
+        rect(this.boundary.x, this.boundary.y, this.boundary.w * 2, this.boundary.h * 2);
+
+        if (this.divided) {
+            this.northWest.show();
+            this.northEast.show();
+            this.southEast.show()
+            this.southWest.show()
+        }
+
+        for (let p of this.points) {
+            strokeWeight(4);
+            point(p.location.x, p.location.y);
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -6,13 +6,14 @@ function setup() {
     let boundary = new Rectangle(width/2, height/2, width/2, height/2);
     let qt = new QuadTree(boundary, 4);
 
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 50; i++) {
         let p = new Point(random(width), random(height));
         qt.insert(p);
     }
-}
 
-function draw() {
-    background(100);
+    background(0);
 
+    qt.show();
+
+    console.log(qt);
 }
